40e9dfac76dba735275d369ede6913791cd7114f
Philip <philip.nguyen@sjsu.edu>
2021-03-07 16:54:03 -0800

    question 4 is done and survey is almost done

--- grading.js
+++ grading.js
@@ -16,3 +16,21 @@ function setQuestion2() {
     // redirect to the next question
     window.location.href("/question3.html");
 }
+
+function setQuestion3() {
+    let input1 = document.getElementById("twt-1-a").checked;
+    let input2 = document.getElementById("twt-2-a").checked;
+
+    sessionStorage.setItem("q3a", input1);
+    sessionStorage.setItem("q3b", input2);
+
+    window.location.href("/question4.html");
+}
+
+function setQuestion4() {
+    let input = document.getElementById("vid-a").checked;
+
+    sessionStorage.setItem("q4", input);
+
+    window.location.href("/survey.html");
+}
